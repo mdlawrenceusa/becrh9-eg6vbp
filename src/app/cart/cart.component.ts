12eaf2571e20adc87d570e315d280c9a8fdf2abd
mdlawrenceusa <mdlawrence@me.com>
2023-09-28 15:53:07 -0400

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class CartComponent {
 
+  items = this.cartService.getItems();
+
+
+  constructor(
+    private cartService: CartService
+  ) { }
 }
