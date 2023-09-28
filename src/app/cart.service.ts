dd449a4dbcf27ca8fced8ba4b322455a9e2e194b
mdlawrenceusa <mdlawrence@me.com>
2023-09-28 15:57:18 -0400

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +21,13 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  constructor() { }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
 }
